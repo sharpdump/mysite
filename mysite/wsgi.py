7aee55d201e3bd6189bb1557de8b17ed623e7740
sharpdump <fake@privacyprotect.com>
2018-04-26 22:46:54 +0800

    initial commit

---
+++ mysite/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+sys.path.insert(0,os.path.join(os.path.dirname(os.path.realpath(__file__)),'..'))
+
+os.environ["DJANGO_SETTINGS_MODULE"]="mysite.settings"
+
+application = get_wsgi_application()
